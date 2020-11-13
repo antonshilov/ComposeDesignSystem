f3ae2d9df0c6bfd1c3eaf7dfdbf18ba6f6c76726
Anton Shilov <vaccum121@gmail.com>
2020-11-13 18:04:18 +0300

    Update interop sample

--- app/src/main/java/com/example/myapplication/samples/ViewComposeInterop.kt
+++ app/src/main/java/com/example/myapplication/samples/ViewComposeInterop.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.ui.tooling.preview.Preview
 import com.example.myapplication.R
@@ -16,24 +15,22 @@ fun ViewComposeInterop() {
 
     val count = remember { mutableStateOf(0) }
 
-    val context = ContextAmbient.current
-
-    val textView = remember {
-        TextView(context).apply {
-            setOnClickListener {
-                count.value = count.value + 1
-            }
-            setTextColor(context.resources.getColor(R.color.white))
-            contentDescription = "CounterAndroidView"
-        }
-    }
-
-
     Column {
         Text(text = "Compose Text Count = ${count.value}")
-        AndroidView(viewBlock = { textView }) { view ->
-            view.text = "Hello From AndroidViews World! Count = ${count.value}"
-        }
+        AndroidView(
+            viewBlock = { context ->
+                TextView(context).apply {
+                    setOnClickListener {
+                        count.value = count.value + 1
+                    }
+                    setTextColor(context.resources.getColor(R.color.white))
+                    contentDescription = "CounterAndroidView"
+                }
+            },
+            update = { view ->
+                view.text = "Hello From AndroidViews World! Count = ${count.value}"
+            }
+        )
     }
 
 }
